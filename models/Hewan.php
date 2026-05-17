5f3b42927b2d1b740be8b396c37cb559afd80155
dianfitriani297 <dianfitrianipuhun@gmail.com>
2026-05-17 21:54:02 +0700

    Add files via upload

--- models/Hewan.php
+++ models/Hewan.php
@@ -1,1 +1,56 @@
+<?php
+require_once __DIR__ . '/../config/koneksi.php';
 
+class Hewan {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function findAll() {
+        $query = mysqli_query($this->conn, "SELECT h.*, k.nama AS nama_kategori 
+                                            FROM hewan h 
+                                            JOIN kategori k ON h.kategori_id = k.id 
+                                            ORDER BY k.nama, h.nama ASC");
+        return mysqli_fetch_all($query, MYSQLI_ASSOC);
+    }
+
+    public function findByKategori($kategoriId) {
+        $kategoriId = intval($kategoriId);
+        $query = mysqli_query($this->conn, "SELECT * FROM hewan WHERE kategori_id = $kategoriId ORDER BY nama ASC");
+        return mysqli_fetch_all($query, MYSQLI_ASSOC);
+    }
+
+    public function findById($id) {
+        $id = intval($id);
+        $query = mysqli_query($this->conn, "SELECT h.*, k.nama AS nama_kategori 
+                                            FROM hewan h 
+                                            JOIN kategori k ON h.kategori_id = k.id 
+                                            WHERE h.id = $id");
+        return mysqli_fetch_assoc($query);
+    }
+
+    public function create($kategoriId, $nama, $info, $gambar) {
+        $kategoriId = intval($kategoriId);
+        $nama       = mysqli_real_escape_string($this->conn, $nama);
+        $info       = mysqli_real_escape_string($this->conn, $info);
+        $gambar     = mysqli_real_escape_string($this->conn, $gambar);
+        return mysqli_query($this->conn, "INSERT INTO hewan (kategori_id, nama, info, gambar) VALUES ($kategoriId, '$nama', '$info', '$gambar')");
+    }
+
+    public function update($id, $kategoriId, $nama, $info, $gambar) {
+        $id         = intval($id);
+        $kategoriId = intval($kategoriId);
+        $nama       = mysqli_real_escape_string($this->conn, $nama);
+        $info       = mysqli_real_escape_string($this->conn, $info);
+        $gambar     = mysqli_real_escape_string($this->conn, $gambar);
+        return mysqli_query($this->conn, "UPDATE hewan SET kategori_id=$kategoriId, nama='$nama', info='$info', gambar='$gambar' WHERE id=$id");
+    }
+
+    public function delete($id) {
+        $id = intval($id);
+        return mysqli_query($this->conn, "DELETE FROM hewan WHERE id=$id");
+    }
+}
+?>
---
+++ models/Kategori.php
@@ -0,0 +1,46 @@
+<?php
+require_once __DIR__ . '/../config/koneksi.php';
+
+class Kategori {
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function findAll() {
+        $query = mysqli_query($this->conn, "SELECT k.*, COUNT(h.id) AS jumlah_hewan 
+                                            FROM kategori k 
+                                            LEFT JOIN hewan h ON h.kategori_id = k.id 
+                                            GROUP BY k.id 
+                                            ORDER BY k.nama ASC");
+        return mysqli_fetch_all($query, MYSQLI_ASSOC);
+    }
+
+    public function findById($id) {
+        $id = intval($id);
+        $query = mysqli_query($this->conn, "SELECT * FROM kategori WHERE id = $id");
+        return mysqli_fetch_assoc($query);
+    }
+
+    public function create($nama, $deskripsi, $gambar) {
+        $nama      = mysqli_real_escape_string($this->conn, $nama);
+        $deskripsi = mysqli_real_escape_string($this->conn, $deskripsi);
+        $gambar    = mysqli_real_escape_string($this->conn, $gambar);
+        return mysqli_query($this->conn, "INSERT INTO kategori (nama, deskripsi, gambar) VALUES ('$nama', '$deskripsi', '$gambar')");
+    }
+
+    public function update($id, $nama, $deskripsi, $gambar) {
+        $id        = intval($id);
+        $nama      = mysqli_real_escape_string($this->conn, $nama);
+        $deskripsi = mysqli_real_escape_string($this->conn, $deskripsi);
+        $gambar    = mysqli_real_escape_string($this->conn, $gambar);
+        return mysqli_query($this->conn, "UPDATE kategori SET nama='$nama', deskripsi='$deskripsi', gambar='$gambar' WHERE id=$id");
+    }
+
+    public function delete($id) {
+        $id = intval($id);
+        return mysqli_query($this->conn, "DELETE FROM kategori WHERE id=$id");
+    }
+}
+?>
