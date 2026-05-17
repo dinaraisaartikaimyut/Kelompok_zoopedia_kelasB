c39d0e349a5e7f25520ce36eb7192667680929b0
dinarkeren <dinaraisaartika@gmail.com>
2026-05-17 22:02:33 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,1 +1,14 @@
+<?php
+$host     = 'localhost';
+$user     = 'root';
+$password = '';
+$database = 'zoopedia';
 
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die('Koneksi gagal: ' . mysqli_connect_error());
+}
+
+mysqli_set_charset($conn, 'utf8');
+?>
