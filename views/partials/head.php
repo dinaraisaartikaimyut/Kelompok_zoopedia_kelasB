18fb2c7272379e03c42ae76dcc5d569e273ea882
dinarkeren <dinaraisaartika@gmail.com>
2026-05-29 19:28:52 +0700

    Update head.php

--- views/partials/head.php
+++ views/partials/head.php
@@ -6,21 +6,6 @@ $isAdmin     = ($currentDir === 'admin');
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<link rel="stylesheet" href="/zoopedia/public/css/main.css" />
-<link rel="stylesheet" href="/zoopedia/public/css/navbar.css" />
-<link rel="stylesheet" href="/zoopedia/public/css/componente.css" />
-
-<?php if ($isAdmin): ?>
-<link rel="stylesheet" href="/zoopedia/public/css/admin.css" />
-<link rel="stylesheet" href="/zoopedia/public/css/detail_hasil.css">
-<?php endif; ?>
-
-<?php if ($currentFile === 'kuis.php'): ?>
-<link rel="stylesheet" href="/zoopedia/public/css/kuis.css" />
-<?php endif; ?>
-
-<?php if (in_array($currentFile, ['kategori.php', 'detail_kategori.php', 'hewan.php'])): ?>
-<link rel="stylesheet" href="/zoopedia/public/css/kategori.css" />
-<?php endif; ?>
+<link rel="stylesheet" href="/zoopedia/public/css/style.css">
 
 <title><?= isset($title) ? htmlspecialchars($title) : 'Zoopedia' ?></title>
